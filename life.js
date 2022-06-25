eb31cdbe937604ef08697325669414018f11204b
Taiwo Alonge <99885991+taiwolonge@users.noreply.github.com>
2022-06-25 15:51:42 +0100

    Add files via upload

---
+++ life.js
@@ -0,0 +1,5 @@
+alert('Introduction to Javascript Task from Zuri')
+
+document.write('Hello, World! My name is Taiwo Alonge.');
+document.write('  My height is 6ft.');
+document.write('  I am from Nigeria.');
